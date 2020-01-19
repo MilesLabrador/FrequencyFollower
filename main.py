5dcb61d47a31a7a12a11a43dc4d9b53fde202e0f
Miles <44444826+MilesLabrador@users.noreply.github.com>
2020-01-18 16:28:17 -0800

    Added obstacle avoidance and signal maximizing algorithm for control of unit

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from signals import detect_sig
+from client import *
 import time
+from motorConfig import *
+from eyeball import *
 
-time.sleep(2)
-print("slept")
-signal_set = detect_sig()
-print("signals")
-print(signal_set)
+car = Car()
+YIELD_DISTANCE = 18 # Minimum distance that we'll keep between sensor and object
+deg_90 = 1
+deg_180 = deg_90 * 2
+
+while True:
+    current_signal = detect_sig()
+    starting_signal = current_signal
+    if get_distance() < YIELD_DISTANCE: # if front blocked
+        car.rotate("left", deg_90) # left rotate
+        if get_distance() < YIELD_DISTANCE: # if left blocked
+            car.rotate("left", deg_180) # 180 turn
+            car.forwards(1) # forward
+        else:
+            car.forwards(1) #forward
+            current_signal = detect_sig()
+            if (current_signal < starting_signal): # if signal is worse
+                car.rotate("left", deg_180)
+                car.forwards(1) # undo last forwards. Backtrack
+                car.forwards(1)
+    else:
+        car.forwards(1)
+        current_signal = detect_sig()
+        if (current_signal < starting_signal): # if signal is worse, turn left and run algorithm
+            car.rotate("left", deg_90)
